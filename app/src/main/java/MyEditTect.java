2f5418543edd29f0399d89e8ea7fd1bf7fcbc48a
Administrator <2868989685@qq.com>
2018-03-29 17:22:49 +0800

    Initial commit

---
+++ app/src/main/java/MyEditTect.java
@@ -0,0 +1,9 @@
+import android.content.Context;
+import android.widget.EditText;
+
+public class MyEditTect extends android.support.v7.widget.AppCompatEditText{
+
+    public MyEditTect(Context context) {
+        super(context);
+    }
+}
